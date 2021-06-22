63287add0a674f91a4b72f854faa979bce2b7e5a
alexsc90 <a.s.cebreros@gmail.com>
2021-06-22 18:23:29 -0500

    Api para usuario agregada y funcionando, swagger agregado

---
+++ api/components/user/controller.js
@@ -0,0 +1,39 @@
+const store = require('../../../store/dummy');
+const nanoid = require('nanoid');
+
+const TABLE = 'user';
+
+module.exports = function(injectedStore) {
+    let store = injectedStore;
+    if(!store) {
+        store = require('../../../store/dummy');
+    }
+
+    function list() {
+        return store.list(TABLE)
+    }
+
+    function get(id) {
+        return store.get(TABLE, id)
+    }
+
+    function upsert(body) {
+        const user = {
+            name: body.name        
+        }
+
+        if(body.id) {
+            user.id = body.id
+        } else {
+            user.id = nanoid();
+        }
+
+        return store.upsert(TABLA, user);
+    }
+
+    return {
+        list, 
+        get,
+        upsert,
+    };
+}
--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,12 +1,48 @@
 const express = require('express');
 
-const response = require('../..//../network/response');
+const response = require('../../../network/response');
+const controller = require('./index');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Todo funciona');
-    response.success(req, res, 'Todo correcto', 200);
+    controller.list()
+        .then((list) => {
+            response.success(req, res, list, 200);
+        })
+        .catch(e => {
+            response.error(req, res, e.message, 500)
+        })
+});
+
+router.get('/:id', (req, res) => {
+    controller.get(req.params.id)
+        .then((user) => {
+            response.success(req, res, user, 200);
+        })
+        .catch(err => {
+            response.error(req, res, err.message, 500)
+        })
+});
+
+router.post('/', (req, res) => {
+    controller.upsert(req.body)
+        .then((user) => {
+            response.success(req, res, user, 201);
+        })
+        .catch(e => {
+            response.error(req, res, e.message, 500);
+        });
+});
+
+router.put('/', (req, res) => {
+    controller.upsert(req.body)
+        .then((user) => {
+            response.success(req, res, user, 201);
+        })
+        .catch(e => {
+            response.error(req, res, e.message, 500);
+        });
 });
 
 module.exports = router;
